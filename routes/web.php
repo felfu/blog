3a5b3f80f3debba2f7000e410e42965df1473097
felfu <felicianfuchs@gmx.de>
2021-09-24 10:54:55 +0200

    add mailchimp api credentials

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,19 @@ use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' =>  config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    $response = $mailchimp->ping->get();
+    ddd($response);
+});
+
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
